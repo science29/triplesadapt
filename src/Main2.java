ec934195461e5c046b54107c29bd0c03b8f06fd5
science29 <powerx.iq@gmail.com>
2019-02-02 12:04:53 +0100

    done the chain query stuff

--- src/Main2.java
+++ src/Main2.java
@@ -1,3 +1,4 @@
+import index.MyHashMap;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.LineIterator;
 import util.FileHashMap;
@@ -980,7 +981,7 @@ public class Main2 {
     }
 
 
-    private Main2.MyHashMap<String, ArrayList<Triple>> sp_O;
+    private MyHashMap<String, ArrayList<Triple>> sp_O;
     private void addTosp_OIndex(Triple triple){
         if (sp_O == null)
             sp_O = new MyHashMap<>("sp_O");
@@ -991,7 +992,7 @@ public class Main2 {
     }
 
 
-    private Main2.MyHashMap<String, ArrayList<Triple>> op_S;
+    private MyHashMap<String, ArrayList<Triple>> op_S;
     private void addToOp_SIndex(Triple triple){
         if (op_S == null)
             op_S = new MyHashMap<>("op_S");
@@ -1000,7 +1001,7 @@ public class Main2 {
         addToIndex(op_S,triple,key);
     }
 
-    private void addToIndex(Main2.MyHashMap<String, ArrayList<Triple>> index , Triple triple , String key){
+    private void addToIndex(MyHashMap<String, ArrayList<Triple>> index , Triple triple , String key){
         if (index.containsKey(key)) {
             index.get(key).add(triple);
         } else {
@@ -1402,147 +1403,7 @@ public class Main2 {
     }
 
 
-    //TODO solve the  creation of the filehashmap
-    private class MyHashMap<K, V> extends HashMap<K, V> implements Serializable {
-
-        private String fileName;
-        private FileHashMap<K, V> fileHashMap;
-        private FileHashMap<K, String> backupFileHashMap;
-        private HashMap<K, V> hashMap;
-        private double elemSize = 0;
-
-        private final long fac = 8;
-        private final long MAX_SIZE_GB =  fac*1000000000;
-
-     //   private int avgElemSize = 1 ;
-
-        public MyHashMap(String fileName) {
-            super();
-            try {
-                fileHashMap = new FileHashMap(fileName);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            hashMap = new HashMap<>();
-            this.fileName = fileName;
-        }
-
-
-        private void setSize() {
-            try {
-                if (hashMap.size() < 10000 || elemSize != 0 || hashMap.size()!=10000 )
-                    return;
-
-                FileWriter fileWriter = new FileWriter("temp_shjf");
-                BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-                int cnt = 0;
-                Iterator it = hashMap.entrySet().iterator();
-                while (it.hasNext()) {
-                    if(cnt > 10000)
-                        break;
-                    Map.Entry pair = (Map.Entry)it.next();
-                    String str = pair.getKey() + " = " + pair.getValue();
-                    fileWriter.write(str);
-                    cnt++;
-                }
-                try {
-                    bufferedWriter.close();
-                    fileWriter.close();
-                } catch (IOException ex) {
-
-                    ex.printStackTrace();
-
-                }
-                File file = new File("temp_shjf");
-                elemSize = (int)file.length()/10000;
-                /*
-                System.out.println("Index Size: " + hashMap.size());
-                ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                ObjectOutputStream oos = new ObjectOutputStream(baos);
-                oos.writeObject(this);
-                oos.close();
-                elemSize = baos.size() / hashMap.size();
-                System.out.println("Data Size: " + baos.size());*/
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-
-
-
-        @Override
-        public boolean containsKey(Object key) {
-            if(hashMap.containsKey(key))
-                return true;
-            if (fileHashMap != null)
-                return fileHashMap.containsKey(key);
-            return false;
-        }
-
-
-        @Override
-        public V get(Object key) {
-            if (hashMap != null && hashMap.containsKey(key))
-                return hashMap.get(key);
-            if (backupFileHashMap != null) {
-                String value = (String) backupFileHashMap.get(key);
-                ArrayList<Triple> tarr = new ArrayList();
-                String elems[] = value.split("algheziAr7");
-                for (int i = 0; i < elems.length; i++) {
-                    String tripleStrA[] = value.split("algheziTr8");
-                    Triple ttriple = new Triple(Long.valueOf(tripleStrA[0]), Long.valueOf(tripleStrA[1]), Long.valueOf(tripleStrA[2]));
-                    tarr.add(ttriple);
-                }
-                return (V) tarr;
-            }
-
-            return fileHashMap.get(key);
-
-        }
-
-
-        @Override
-        public V put(K key, V value) {
-            setSize();
-            if (elemSize * hashMap.size() < MAX_SIZE_GB)
-                return hashMap.put(key, value);
-
-            if (value instanceof ArrayList) {
-                ArrayList tarr = (ArrayList) value;
-                if (tarr.size() > 0 && tarr.get(0) instanceof Triple) {
-                    String serilaizedArray = "";
-                    for (int i = 0; i < tarr.size(); i++) {
-                        Triple ttriple = (Triple) tarr.get(i);
-                        String tripleStr = ttriple.triples[0] + "algheziTr8" + ttriple.triples[1] + "algheziTr8" + ttriple.triples[2];
-                        if (i == 0)
-                            serilaizedArray = tripleStr;
-                        else
-                            serilaizedArray = serilaizedArray + "algheziAr7" + tripleStr;
-                    }
-                    if (backupFileHashMap == null)
-                        try {
-                            backupFileHashMap = new FileHashMap("backup" + fileName);
-
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    return (V) backupFileHashMap.put(key, serilaizedArray);
-                }
-            }
-            if (fileHashMap == null)
-                try {
-                    fileHashMap = new FileHashMap(fileName);
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            return fileHashMap.put(key, value);
-
-
-        }
-
-    }
+  
 
 
 
---
+++ src/index/IndexColl.java
@@ -0,0 +1,19 @@
+package index;
+
+public class IndexColl {
+
+
+
+    public static int S = 1;
+    public static int P = 2;
+    public static int O = 3;
+    public static int N = 0;
+
+
+
+    public class IndexType{
+        int [] type ;
+    }
+
+
+}
